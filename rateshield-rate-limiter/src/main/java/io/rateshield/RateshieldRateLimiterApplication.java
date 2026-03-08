7b3a5830eacc007f43496b142208d415e62b7ffd
iamabhishekmaurya <iamabhishekmaurya@gmail.com>
2026-03-09 00:54:45 +0530

    basic setup

---
+++ rateshield-rate-limiter/src/main/java/io/rateshield/RateshieldRateLimiterApplication.java
@@ -0,0 +1,13 @@
+package io.rateshield;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RateshieldRateLimiterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RateshieldRateLimiterApplication.class, args);
+	}
+
+}
---
+++ rateshield-rate-limiter/src/test/java/io/rateshield/RateshieldRateLimiterApplicationTests.java
@@ -0,0 +1,13 @@
+package io.rateshield;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RateshieldRateLimiterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
